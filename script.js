3ac6a88e2733a5bc25141138190c589fd8361323
riyaaaa19 <riyasaraf19@gmail.com>
2024-12-23 22:11:18 +0530

    Initial commit for HealthQ

---
+++ script.js
@@ -0,0 +1,12 @@
+function openTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+      tab.classList.remove('active');
+    });
+  
+    const selectedTab = document.getElementById(tabId);
+    if (selectedTab) {
+      selectedTab.classList.add('active');
+    }
+  }
+  
